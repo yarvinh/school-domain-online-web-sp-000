c4bfe564f711c792333f651e72e2809187508b37
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-11 03:13:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,7 +24,7 @@ class School
       @sorting = {}
        keys =  @roster.keys.sort
       @roster.map {|by_key,by_value|
-       values =  @roster[by_key]
+       values =  @roster[keys]
         if @sorting.has_key?(keys[counter])
            @sorting[keys[counter]] << values.join(",")
          else
